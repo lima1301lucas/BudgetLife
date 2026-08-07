c31baa502fbac257a280148c03027284dadd2c17
Lucas Oliveira <lima1301lucas@gmail.com>
2026-08-06 21:19:23 -0300

    feat(repositories): cria CategoryRepository e ExpenseRepository

--- Models/Expense.cs
+++ Models/Expense.cs
@@ -10,7 +10,7 @@ namespace BudgetLife.Models
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public float Amount { get; set; }
+        public decimal Amount { get; set; }
         public DateTime Date { get; set; }
         public int CategoryId {  get; set; }
         public Category? Category { get; set; }
---
+++ Repositories/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BudgetLife.Models;
+
+namespace BudgetLife.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Category? GetById(int id);
+        List<Category> GetAll();
+        void Add(Category category);
+        void Update(Category category);
+        void Delete(int id);
+    }
+}
