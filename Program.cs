a2ad7181dc0a181ec68170bf2272948c077ce5e2
Lucas Oliveira <lima1301lucas@gmail.com>
2026-08-10 18:16:33 -0300

    feat: compõe dependências no Program.cs e corrige tipo no construtor do ExpenseService

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using BudgetLife.Data;
+using BudgetLife.Repositories;
+using BudgetLife.Services;
+using BudgetLife.UI;
+
+var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false).Build();
+
+string connectionString = config.GetConnectionString("Default")!;
+var dbConnection = new DatabaseConnection(connectionString);
+using var connection = dbConnection.CreateConnection();
+
+var categoryRepository = new CategoryRepository(dbConnection);
+var expenseRepository = new ExpenseRepository(dbConnection);
+var reportRepository = new ReportRepository(dbConnection);
+
+var categoryService = new CategoryService(categoryRepository, expenseRepository);
+var expenseService = new ExpenseService(expenseRepository, categoryRepository);
+var reportService = new ReportService(reportRepository);
+
+var menu = new MenuPrincipal(expenseService, categoryService, reportService);
+menu.Init();
